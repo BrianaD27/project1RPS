2e5a29ca04643a64498094446a3143593ffc03c6
Briana Davis <brianad570@gmail.com>
2025-09-24 18:39:26 -0400

    Created Menu Function

---
+++ rps.py
@@ -0,0 +1,11 @@
+import random
+
+def menu_screen():
+    global answer
+
+    print("=================================")
+    print("Welcome to Rock Paper Scissors!")
+    print(" ")
+    answer = input("Are you ready to start? (Y/N)? ")
+
+
